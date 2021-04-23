26225cad1eac4ef100ce6e954c91531bdb28940d
fr0g2s <turttle2s@naver.com>
2021-04-24 05:20:15 +0900

    +)_+

--- v.0.1/bugFinder.py
+++ v.0.1/bugFinder.py
@@ -1,51 +1,55 @@
 import angr, claripy
+import exploitGen
 
-# frame
 class bugFinder:    
-    def __init__(self, bin_name, isStdin=None, max_len=0, isArgs=None, args_cnt):
-        self.isStripped = False
-        self.main_addr = None
-        self.isArgs = isArgs
-        self.args = [bin_name]
-        self.args_cnt = args_cnt
-        self.isStdin = isStdin
-        self.len = max_len  # all input data's length have max_len (heuristic)
-        self.stdin_data = []
+    """
+        find unconstrained state with forward-SE.
+        check if bp,ip register is symbolic.
+        for now, no strategy for efficient explore
 
-    def getFuncAddr(self, cfg, func_name):
-        for addr, func in cfg.kb.functions.items():
-            if func_name == func.name:
-                return addr
-        return None
-    
-    def setProj(self):
-        self.proj = angr.Project(self.bin_name, load_options={'auto_load_libs':False})
-        cfg = self.proj.analyses.CFG(fail_fast=True)
-        self.main_addr = self.getFuncAddr(cfg, 'main')        
-        if self.main_addr is None:
-            self.isStripped = True    
-        self.setState()
+        Usage:
+        bugFinder(arch, exploitType={}, strategy)
+        - arch: target bin's arch info
+        - exploitType: what exploit type to find
+            -- stack 
+                = return to stack
+                = return to libc
+                = return to code
+            -- heap
+                = None
+        - strategy: exploring strategy.
+            -- None
+    """
 
-    def setState(self):
-        if self.isStdin is True:
-            self.setStdin()
-        if self.isArgs is True:
-            self.setArgs()
+    def __init__(self, arch, exploitType={},strategy=None):
+        self.arch = arch    # x86, AMD64, 
+        self.exploitType = exploitType
+        self.strategy = strategy
 
-        if isStripped is True:  # if stripped binary, use entry_state()
-            self.state = self.proj.factory.entry_state(args=self.args, stdin=self.data)
-        else:   # if we can use symbol info, use call_state(main)
-            self.state = self.proj.factory.call_state(self.main_addr, args=self.args, stdin=angr.SimFile('/dev/stdin',self.stdin_data))
+    def getExploitableState(self, simgr):
+        """
+            return unconstrained(exploitable) state
+        """
+        while len(simgr.unconstrained) == 0:
+            simgr.step()
 
-        self.setConstraints()
+        exploitableState = []
+        if len(simgr.unconstrained) > 0:
+            print('[*] found %d unconstrained state' % len(simgr.unconstrained))
+            for ust in simgr.unconstrained:
+                if not self.isExploitable(ust):
+                    continue
+                exploitableState.append(ust)
+        if len(exploitableState) > 0:
+            return exploitableState
+        else:
+            return False
 
-    def setStdin(self):
-        self.stdin_data = claripy.BVS('stdin_data', 8 * self.len)
-    
-    def setArgs(self):
-        for i in range(0, self.args_cnt, 1):
-            self.args.append(claripy.BVS('args_%d' i, 8*self.len))
-
-    def setConstraints(self):   # not null ?
-        pass
-
+    def isExploitable(self, state):
+        """
+            check only ip register's controllability
+        """
+        ip = state.pc.args[0]
+        if state.solver.symbolic(ip) is True:
+            return True
+        return False
